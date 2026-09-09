52a3d1f8b9191325f8540d54d52d6ff93bf4d0b6
eugen <lazoskal24@gmail.com>
2026-09-09 18:27:55 +0300

    reconfigured hhyperparams

--- instanseg/utils/AI_utils.py
+++ instanseg/utils/AI_utils.py
@@ -179,7 +179,7 @@ class Segmentation_Dataset(Dataset):
         data, label = augmented['image'], augmented['mask']
         data = data.to(torch.float32) / 255.0
         label = label.unsqueeze(0).to(torch.int64)
-        if i == 1:
+        if i == 0:
             print(f"Data - shape: {data.shape}, min: {data.min()}, max: {data.max()}, dtype: {data.dtype}, some unique values: {torch.unique(data)[:5]}")
             print(f"Label - shape: {label.shape}, min: {label.min()}, max: {label.max()}, dtype: {label.dtype}, some unique values: {torch.unique(label)[:5]}")
             import matplotlib.pyplot as plt
